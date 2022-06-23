c9589429d1edf36aa947291b8cfde4a6ed89d568
Jan Geicke <jan.geicke@mni.thm.de>
2022-06-23 13:52:51 +0200

    adjusted ingredient retrieval and added temporary ingredients for testing purposes

--- src/app/services/open-food-facts.service.ts
+++ src/app/services/open-food-facts.service.ts
@@ -10,10 +10,86 @@ export class OpenFoodFactsService {
 
   public successfulScan;
   public scannedHistamineFreeProduct = false;
+  public ingredientsFound;
   private scannedProductName = '';
 
-  private histamineFood = ['strawberries', 'raspberries','acid', 'lemon', 'orange', 'citrus', 'banana',
-                          'pineapple', 'kiwi', 'pear', 'papaya', 'guava',];
+  private histamineFood = ['bacterial-culture',	'culture-bacterienne',	'bifidobacterium-lactis',
+    'yeast-and-bacteria-cultures' ,	'bifidobacterium-bifidum',
+    'alcohol',	'alcohol-vinegar', 	'cetearyl-alcohol', 	'sugar-alcohol', 	'grain-alcohol',
+    'vinegar',
+    'yeast',
+    'soy-milk',
+    'theobroma-cacao',
+    'nettle',
+    'cocoa',
+    'chocolate',
+    'carob-seeds',	'carob-seed-flour', 	'carob-bean',
+    'soy-sauce',
+    'glutamate-monosodique', 	'flavour-enhancer',
+    'strawberry',
+    'raspberry',
+    'lemon',
+    'orange',
+    'tangerine',	'clementine',
+    'grapefruit',
+    'lime',
+    'banana',
+    'pineapple',
+    'kiwi',
+    'pear',
+    'papaya',
+    'guava',
+    'nut',
+    'sauerkraut',
+    'spinach',
+    'tomato', 	'ketchup',
+    'aubergine',
+    'avocado',
+    'olives',
+    'lentils',
+    'beans',
+    'soya-lecithin',
+    'cep',
+    'morels',
+    'hard-cheese',
+    'soft-cheese',
+    'pasteurized-processed-cheese',
+    'blue-cheese ',
+    'mold-inhibitor',
+    'tuna',
+    'mackerel',
+    'herring',
+    'sardine',
+    'anchovy',
+    'mahi-mahi',
+    'mussel',
+    'lobster',
+    'crab',
+    'shrimp', 	'prawn',
+    'sausage',
+    'salami',
+    'liver',
+    'tongue',
+    'heart',
+    'rice-milk',
+    'oat-milk',
+    'black-tea',
+    'coffee',
+    'green-tea',
+    'espresso',
+    'walnut-oil',
+    'malt',
+    'wheat-germ',
+    'mushroom',
+    'raw-milk',
+    'yogurt',
+    'kefir-ferments', 	'kefir',
+    'soured-milk ',
+    'buttermilk',
+    'sour-cream',
+    'creme-fraiche',
+    'feta',
+  ];
 
   constructor(private httpClient: HttpClient) { }
 
@@ -31,42 +107,56 @@ export class OpenFoodFactsService {
   }
 
   public getIngredients(barcode: string){
-    // TODO: error handling
-    return new Promise((resolve, reject) => {
-      const promise = this.httpClient.get('https://world.openfoodfacts.org/api/v2/product/'+barcode,
-        {observe: 'body', responseType: 'text'}).toPromise();
-      promise.then((data) => {
-        const response = JSON.parse(data);
+    // TODO: error handling (http 404)
+    return new Promise((resolve) => {
+      this.httpClient.get('https://world.openfoodfacts.org/api/v2/product/'+barcode,
+        {observe: 'body', responseType: 'text'}).subscribe(result => {
+          const response = JSON.parse(result);
+          //check if product was found in OpenFoodFacts database
+          this.successfulScan = response.status;
+          if(!this.successfulScan){
+            resolve(1);
+            return;
+          }
 
-        //check if product was found in OpenFoodFacts database
-        this.successfulScan = response.status;
-        if(!this.successfulScan){
-          resolve(0);
-        }
+          // get product name
+          this.scannedProductName = '';
+          this.scannedProductName = response.product.product_name_de;
+          if(this.scannedProductName === undefined){
+            this.scannedProductName = response.product.product_name;
+          }
 
-        const ingredients = response.product.ingredients;
+          // get ingredients
+          const ingredients = response.product.ingredients;
 
-        // get product name
-        this.scannedProductName = '';
-        this.scannedProductName = response.product.product_name_de;
-        if(this.scannedProductName === undefined){
-          this.scannedProductName = response.product.product_name;
-        }
+          if(ingredients === undefined){
+            this.ingredientsFound = false;
+            resolve(1);
+            return;
+          }
+          this.ingredientsFound = true;
 
-        // get ingredients
-        let histamineHits = 0;
-        ingredients.forEach(ingredient => {
-          this.histamineFood.forEach((histaminFood) => {
-            if(ingredient.id.includes(histaminFood)){
-              console.log('ALARM!');
-              histamineHits++;
-            }
+          // get ingredients
+          let histamineHits = 0;
+          ingredients.forEach(ingredient => {
+            this.histamineFood.forEach((histaminFood) => {
+              if(ingredient.id.includes(histaminFood)){
+                console.log('ALARM!');
+                histamineHits++;
+              }
+            });
           });
-        });
 
-        this.scannedHistamineFreeProduct = histamineHits === 0;
-        resolve(1);
-      });
+          this.scannedHistamineFreeProduct = histamineHits === 0;
+          resolve(0);
+      },
+        error => {
+          console.log(error);
+          this.ingredientsFound = false;
+          this.successfulScan = false;
+          resolve(1);
+          return;
+        });
     });
   }
 }
